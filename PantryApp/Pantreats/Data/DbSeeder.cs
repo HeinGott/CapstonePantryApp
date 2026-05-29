2545b5e4bc9fffd2b5f1977ac8f552acbd3b93a0
Nicholas Chapman-Miller <nicholaschapmanm@gmail.com>
2026-05-29 18:14:57 -0400

    Catch up to Main

---
+++ PantryApp/Pantreats/Data/DbSeeder.cs
@@ -0,0 +1,36 @@
+﻿namespace Pantreats.Data
+{
+    public class DbSeeder
+    {
+        public static async Task SeedAdminAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            //check admin role exists -nick
+            if (!await roleManager.RoleExistsAsync("Admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+           
+            var adminEmail = "admin@admin.com";
+            var adminPassword = "Admin_123";
+
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(adminUser, adminPassword);
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+
+    }
+}

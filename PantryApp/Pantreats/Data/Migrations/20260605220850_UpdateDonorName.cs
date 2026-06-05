2e137754262ab121ffaca237319c832cd676d45f
Nicholas Chapman-Miller <nicholaschapmanm@gmail.com>
2026-06-05 18:13:53 -0400

    Add Missing Migration for Donor Name Change

---
+++ PantryApp/Pantreats/Data/Migrations/20260605220850_UpdateDonorName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pantreats.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDonorName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VendorID",
+                table: "Vendors",
+                newName: "DonorID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DonorID",
+                table: "Vendors",
+                newName: "VendorID");
+        }
+    }
+}

984fe291d2182a7d32730c712c705dce3ebb24a3
Heinrich Gottberg <heinrichg16@yahoo.com>
2026-06-23 16:42:55 -0400

    Added phone number to register page

--- PantryApp/Pantreats/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PantryApp/Pantreats/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,6 +107,8 @@ namespace Pantreats.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
+                user.PhoneNumber = Input.PhoneNumber?.Trim();
+
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);

42db82fb587e6e8ec862e74504913efb87e26b72
BFrit10 <bfritsche01@gmail.com>
2026-07-08 11:33:57 -0400

    Adding more support articles
    More will come

--- PantryApp/Pantreats/Data/SupportArticleConfiguration.cs
+++ PantryApp/Pantreats/Data/SupportArticleConfiguration.cs
@@ -47,6 +47,36 @@ namespace Pantreats.Data
                     "\nStep 2: Click the User Management button in the navbar." +
                     "\nStep 3: Click the dropdown menu under Actions for any user and change to the desired role." +
                     "\nStep 4: Click the Save button next to the modified user when finished."
+                },
+                new SupportArticle
+                {
+                    Id = 4,
+                    Title = "How to Create an Account",
+                    Slug = "create-account",
+                    Keywords = new[] { "create", "account" },
+                    Summary = "Learn how to create an account on Pantreats.",
+                    Content = "Step 1: Click the Register button on the navbar." +
+    "\nStep 2: From the Register page, click 'Student' or 'Donor' based on the type of account you wish to make." +
+    "\nStep 3: Fill out the required fields, then press the Register button to proceed to the next page based on the role you chose to sign up with."
+                },
+                new SupportArticle
+                {
+                    Id = 5,
+                    Title = "How to Make a Donation",
+                    Slug = "make-donation",
+                    Keywords = new[] { "donate", "donation" },
+                    Summary = "Learn how to donate to Pantreats.",
+                    Content = "Step 1: Log in as a donor. \nStep2: Click the 'Make a Donation' button on the dashboard." +
+                    "\nStep 3: From the Make a Donation page, select the item categories you would like to donate to and enter in the desired quantity. \nStep 4: Enter a donation pickup/dropoff address and/or a comment about the donation (both of these are optional). \nStep 5: Click the 'Submit Donation' button when finished to submit your donation."
+                },
+                new SupportArticle
+                {
+                    Id = 6,
+                    Title = "How to Add a Recipe",
+                    Slug = "add-recipe",
+                    Keywords = new[] { "recipe", "recipes" },
+                    Summary = "Learn how to add a recipe to Pantreats.",
+                    Content = "Step 1: Log in as an admin. \nStep 2: Click the Recipes button on the navbar. \nStep 3: From the Recipes page, click the Add Recipe button. \nStep 4: Fill out the fields (recipe title, meal type, instructions, image, dietary information, ingredients) then either press Preview Recipe to see what your recipe will look like or Save Recipe to add the recipe."
                 });
         }
     }
